f29e9bf2a8288afca1d9795fe4e8cfd5ca2f1c46
roger <maitebst@alumnes.ilerna.com>
2025-11-03 19:02:54 +0100

    cracion del codigo
    Codigo de programacion

---
+++ src/caos/calculadora.java
@@ -0,0 +1,14 @@
+package caos;
+
+public class calculadora {
+	    public static void main(String[] args) {
+	        System.out.println("=== Calculadora Básica ===");
+	        int a = 10;
+	        int b = 5;
+
+	        System.out.println("Suma: " + (a + b));
+	        System.out.println("Resta: " + (a - b));
+	        System.out.println("Multiplicación: " + (a * b));
+	        System.out.println("División: " + (a / b));
+	    }
+	}
